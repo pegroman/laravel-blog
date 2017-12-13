08c785d70c4ce77c73436c14314a3948d225b985
pegroman <roman.alejandro.b@gmail.com>
2017-12-13 15:22:07 -0300

    solucion de redireccion login

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::post('/posts/{post}/comments','CommentsController@store');
 Route::get('/register', 'RegistrationController@create');
 Route::post('/register', 'RegistrationController@store');
 
-Route::get('/login', 'SessionsController@create');
+Route::get('/login', 'SessionsController@create')->name('login');
 Route::post('/login','SessionsController@store');
 Route::get('/logout','SessionsController@destroy');
